4d4147c731040f491d371bd096a0413c96f3959b
Andy Blower <andy.blower@worldpay.com>
2018-03-06 15:45:37 +0000

    Add locator item class

---
+++ Archaeologists/Scripts/LocatorItem.cs
@@ -0,0 +1,31 @@
+﻿// Project:         Archaeologists Guild for Daggerfall Unity (http://www.dfworkshop.net)
+// Copyright:       Copyright (C) 2018 Hazelnut
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Author:          Hazelnut
+using System;
+using DaggerfallWorkshop.Game.Items;
+using UnityEngine;
+
+namespace Archaeologists
+{
+    public class LocatorItem : DaggerfallUnityItem
+    {
+        public virtual bool IsStackable()
+        {
+            return true;
+        }
+
+        public override bool UseItem(ItemCollection collection)
+        {
+            Debug.Log("Using locator item!");
+
+            if (stackCount > 1)
+                stackCount--;
+            else
+                collection.RemoveItem(this);
+            
+            return true;
+        }
+    }
+}
+
