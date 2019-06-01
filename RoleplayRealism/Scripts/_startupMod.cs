f0853f2fdf9268b362b8cafd2d6b964299140288
Andy Blower <andyblower@gmail.com>
2019-06-01 20:47:25 +0100

    Add basic bow drawing tohit and damage scaling

--- RoleplayRealism/Scripts/_startupMod.cs
+++ RoleplayRealism/Scripts/_startupMod.cs
@@ -4,6 +4,9 @@
 // Author:          Hazelnut
 
 using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Entity;
+using DaggerfallWorkshop.Game.Formulas;
+using DaggerfallWorkshop.Game.Items;
 using DaggerfallWorkshop.Game.UserInterface;
 using DaggerfallWorkshop.Game.UserInterfaceWindows;
 using DaggerfallWorkshop.Game.Utility.ModSupport;
@@ -40,6 +43,10 @@ namespace RoleplayRealism
             PlayerActivate.RegisterModelActivation(41001, BedActivation);
             PlayerActivate.RegisterModelActivation(41002, BedActivation);
 
+            // Override adjust to hit mod and damage formulas
+            FormulaHelper.formula_2de_2i.Add("AdjustWeaponHitChanceMod", AdjustWeaponHitChanceMod);
+            FormulaHelper.formula_2de_2i.Add("AdjustWeaponAttackDamage", AdjustWeaponAttackDamage);
+
             Debug.Log("Finished mod init: RoleplayRealism");
         }
 
@@ -49,6 +56,56 @@ namespace RoleplayRealism
             IUserInterfaceManager uiManager = DaggerfallUI.UIManager;
             uiManager.PushWindow(new DaggerfallRestWindow(uiManager, true));
         }
+
+        private static int AdjustWeaponHitChanceMod(DaggerfallEntity attacker, DaggerfallEntity target, int hitChanceMod, int weaponAnimTime, DaggerfallUnityItem weapon)
+        {
+            if (weaponAnimTime > 0 && (weapon.TemplateIndex == (int)Weapons.Short_Bow || weapon.TemplateIndex == (int)Weapons.Long_Bow))
+            {
+                int adjustedHitChanceMod = hitChanceMod;
+                if (weaponAnimTime < 200)
+                    adjustedHitChanceMod -= 40;
+                else if (weaponAnimTime < 500)
+                    adjustedHitChanceMod -= 10;
+                else if (weaponAnimTime < 1000)
+                    adjustedHitChanceMod = hitChanceMod;
+                else if (weaponAnimTime < 2000)
+                    adjustedHitChanceMod += 10;
+                else if (weaponAnimTime > 4000)
+                    adjustedHitChanceMod -= 10;
+                else if (weaponAnimTime > 6000)
+                    adjustedHitChanceMod -= 20;
+
+                Debug.LogFormat("Adjusted Weapon HitChanceMod for bow drawing from {0} to {1} (t={2}ms)", hitChanceMod, adjustedHitChanceMod, weaponAnimTime);
+                return adjustedHitChanceMod;
+            }
+
+            return hitChanceMod;
+        }
+
+        private static int AdjustWeaponAttackDamage(DaggerfallEntity attacker, DaggerfallEntity target, int damage, int weaponAnimTime, DaggerfallUnityItem weapon)
+        {
+            if (weaponAnimTime > 0 && (weapon.TemplateIndex == (int)Weapons.Short_Bow || weapon.TemplateIndex == (int)Weapons.Long_Bow))
+            {
+                double adjustedDamage = damage;
+                if (weaponAnimTime < 1000)
+                    adjustedDamage *= (double)weaponAnimTime / 1000;
+                else if (weaponAnimTime > 4000)
+                    adjustedDamage *= 0.85;
+                else if (weaponAnimTime > 6000)
+                    adjustedDamage *= 0.75;
+                else if (weaponAnimTime > 8000)
+                    adjustedDamage *= 0.5;
+                else if (weaponAnimTime > 10000)
+                    adjustedDamage *= 0.25;
+
+                Debug.LogFormat("Adjusted Weapon Damage for bow drawing from {0} to {1} (t={2}ms)", damage, (int)adjustedDamage, weaponAnimTime);
+                return (int)adjustedDamage;
+            }
+            Debug.Log("AdjustWeaponAttackDamage for bow drawing. " + damage);
+
+            return damage;
+        }
+
 
     }
 }
