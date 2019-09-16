2b48108496653620c6953297766d464fa05a5b0a
Andy Blower <andyblower@gmail.com>
2019-09-16 11:18:08 +0100

    Change horse angle based on average terrain gradient. Knockback charged enemies.

--- RoleplayRealism/Scripts/EnhancedRiding.cs
+++ RoleplayRealism/Scripts/EnhancedRiding.cs
@@ -18,11 +18,15 @@ namespace DaggerfallWorkshop.Game
         public float extW = 0.78f;
 
         const int nativeScreenHeight = 200;
+        const int samples = 16;
+
+        int sampleIdx = 0;
+        float[] terrainAngles = new float[samples];
 
         PlayerMotor playerMotor;
         TransportManager transportManager;
+        PlayerMouseLook playerMouseLook;
 
-        bool lastRiding;
         GameObject cachedColliderHitObject;
 
 
@@ -38,12 +42,15 @@ namespace DaggerfallWorkshop.Game
             playerMotor = GetComponent<PlayerMotor>();
             if (!playerMotor)
                 throw new Exception("PlayerMotor not found.");
-            lastRiding = playerMotor.IsRiding;
 
             transportManager = GetComponent<TransportManager>();
             if (!transportManager)
                 throw new Exception("TransportManager not found.");
             transportManager.DrawHorse = false;
+
+            playerMouseLook = GameManager.Instance.PlayerMouseLook;
+            if (!playerMouseLook)
+                throw new Exception("PlayerMouseLook not found.");
 
             GameManager.Instance.SpeedChanger.CanRun = CanRunUnlessRidingCart;
         }
@@ -51,13 +58,23 @@ namespace DaggerfallWorkshop.Game
         // Update the mouse look pitch limit when riding status changes.
         void Update()
         {
-            if (lastRiding != playerMotor.IsRiding)
+            if (!GameManager.IsGamePaused && playerMotor.IsRiding)
             {
-                // Set min pitch.
-                if (lastRiding = playerMotor.IsRiding)
-                    GameManager.Instance.PlayerMouseLook.PitchMaxLimit = 18;
-                else
-                    GameManager.Instance.PlayerMouseLook.PitchMaxLimit = PlayerMouseLook.PitchMax;
+                // Sample angle of terrain
+                Ray ray = new Ray(transform.position, Vector3.down);
+                RaycastHit hit1, hit2;
+                Physics.Raycast(transform.position, Vector3.down, out hit1);
+                Physics.Raycast(transform.position + transform.forward, Vector3.down, out hit2);
+                float heightDiff = hit1.point.y - hit2.point.y;
+                float angle = Mathf.Atan2(heightDiff, 1) * 100;
+                terrainAngles[sampleIdx++] = angle;
+                if (sampleIdx >= samples)
+                    sampleIdx = 0;
+                //Debug.LogFormat("Angle: {0}", angle);
+            }
+            else
+            {
+                playerMouseLook.PitchMaxLimit = PlayerMouseLook.PitchMax;
             }
         }
 
@@ -83,7 +100,7 @@ namespace DaggerfallWorkshop.Game
                     {
                         GameObject guard = playerEntity.SpawnCityGuard(mobileNpc.transform.position, mobileNpc.transform.forward);
                         DaggerfallEntityBehaviour guardBehaviour = guard.GetComponent<DaggerfallEntityBehaviour>();
-                        HandleCharge(guard, guardBehaviour);
+                        HandleCharge(guard, guardBehaviour, transform.forward);
                     }
                     playerEntity.CrimeCommitted = PlayerEntity.Crimes.Assault;  // Nearest to manslaughter
                     mobileNpc.Motor.gameObject.SetActive(false);
@@ -98,22 +115,20 @@ namespace DaggerfallWorkshop.Game
             {
                 DaggerfallEntityBehaviour hitEntityBehaviour = other.gameObject.GetComponent<DaggerfallEntityBehaviour>();
                 if (playerMotor.IsRiding && playerMotor.IsRunning && hitEntityBehaviour)
-                    HandleCharge(other.gameObject, hitEntityBehaviour);
+                    HandleCharge(other.gameObject, hitEntityBehaviour, other.moveDirection);
                 else
                     cachedColliderHitObject = other.gameObject;
             }
         }
 
         // Handle charging into enemies.
-        private void HandleCharge(GameObject hitGO, DaggerfallEntityBehaviour hitEntityBehaviour)
+        private void HandleCharge(GameObject hitGO, DaggerfallEntityBehaviour hitEntityBehaviour, Vector3 direction)
         {
             if (hitEntityBehaviour.Entity is EnemyEntity)
             {
                 EnemyEntity hitEnemyEntity = (EnemyEntity)hitEntityBehaviour.Entity;
                 if (!hitEnemyEntity.PickpocketByPlayerAttempted)
                 {
-                    Debug.LogFormat("Charged down a {0}!", hitEntityBehaviour.name);
-
                     // Play heavy hit sound.
                     EnemySounds enemySounds = hitGO.GetComponent<EnemySounds>();
                     DaggerfallMobileUnit entityMobileUnit = hitGO.GetComponentInChildren<DaggerfallMobileUnit>();
@@ -124,12 +139,19 @@ namespace DaggerfallWorkshop.Game
                         gender = Genders.Female;
                     enemySounds.PlayCombatVoice(gender, false, true);
 
-                    // Handle charge hit.
+                    // Knockback the enemy.
+                    EnemyMotor enemyMotor = hitGO.GetComponent<EnemyMotor>();
+                    enemyMotor.KnockbackSpeed = 100;
+                    enemyMotor.KnockbackDirection = direction;
+
+                    // Handle charge hit damage.
                     hitEnemyEntity.PickpocketByPlayerAttempted = true;
                     DaggerfallEntityBehaviour playerEntityBehaviour = GameManager.Instance.PlayerEntity.EntityBehaviour;
                     int damage = FormulaHelper.CalculateHandToHandMaxDamage(GameManager.Instance.PlayerEntity.Skills.GetLiveSkillValue(DFCareer.Skills.HandToHand));
-                    hitEntityBehaviour.DamageHealthFromSource(playerEntityBehaviour, damage * 2, true, BloodPos());
+                    damage = playerMotor.IsRunning ? damage * 2 : damage;
+                    hitEntityBehaviour.DamageHealthFromSource(playerEntityBehaviour, damage, true, BloodPos());
                     GameManager.Instance.PlayerEntity.DecreaseFatigue(PlayerEntity.DefaultFatigueLoss * 15);
+                    Debug.LogFormat("Charged down a {0} for {1} damage!", hitEntityBehaviour.name, damage);
                 }
             }
         }
@@ -153,12 +175,14 @@ namespace DaggerfallWorkshop.Game
                     float horseScaleY = (float)Screen.height / (float)nativeScreenHeight;
                     float horseScaleX = horseScaleY * TransportManager.ScaleFactorX;
 
-                    float yAdj = 0;
-                    PlayerMouseLook playerMouseLook = GameManager.Instance.PlayerMouseLook;
-                    if (playerMouseLook)
-                    {
-                        yAdj = (playerMouseLook.Pitch - 10) * LookPitchRatio;
-                    }
+                    // Calculate average terrain angle
+                    float terrainAngle = 0;
+                    for (int i=0; i < samples; i++)
+                        terrainAngle += terrainAngles[i];
+                    terrainAngle /= samples;
+                    // Set min look pitch and calc horse sprite y position adjustment
+                    playerMouseLook.PitchMaxLimit = terrainAngle + 18;
+                    float yAdj = (playerMouseLook.Pitch - terrainAngle - 10) * LookPitchRatio;
 
                     // Calculate position for horse texture and draw it.
                     Rect pos = new Rect(
